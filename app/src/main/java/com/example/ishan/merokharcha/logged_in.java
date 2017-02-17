2990f1f8d06e86547604405a32e21d7d6f155947
iamishang <iamishan9@gmail.com>
2017-02-17 21:09:00 +0545

    Popup

--- app/src/main/java/com/example/ishan/merokharcha/logged_in.java
+++ app/src/main/java/com/example/ishan/merokharcha/logged_in.java
@@ -3,18 +3,38 @@ package com.example.ishan.merokharcha;
 /**
  * Created by Ishan on 2/13/2017.
  */
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class logged_in extends AppCompatActivity {
+
+    Button button3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.logged_in);
-    }
+
+
+        button3 =(Button)findViewById(R.id.button3);
+
+        button3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getApplicationContext(), add_trans.class);
+                startActivity(i);
+            }
 
 
 
 
-}
+
+
+
+
+    });
+}}

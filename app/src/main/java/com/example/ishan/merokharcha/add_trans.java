a97cf29cc952fce55d0072e4fb918386ac9c681a
iamishang <iamishan9@gmail.com>
2017-05-10 15:07:32 +0545

    database??

--- app/src/main/java/com/example/ishan/merokharcha/add_trans.java
+++ app/src/main/java/com/example/ishan/merokharcha/add_trans.java
@@ -3,12 +3,23 @@ package com.example.ishan.merokharcha;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static android.support.constraint.R.id.parent;
 
 /**
  * Created by Ishan on 2/17/2017.
  */
 
-public class add_trans extends AppCompatActivity {
+public class add_trans extends AppCompatActivity implements OnItemSelectedListener{
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +32,28 @@ public class add_trans extends AppCompatActivity {
         int width = dm.widthPixels;
         int height= dm.heightPixels;
 
-        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+        getWindow().setLayout((int)(width*.9),(int)(height*.9));
+
+        spinner = (Spinner)findViewById(R.id.spinner);
+        adapter = ArrayAdapter.createFromResource(this,R.array.expenditure_type,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(this);
 
 
+    }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        String selected= parent.getItemAtPosition(position).toString();
+        Toast.makeText(this,selected,Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
